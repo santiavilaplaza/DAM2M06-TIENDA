c45c3f78eeda1907c303f362ef00ff010c0621f0
santiavilaplaza <santiagoavilaplaza@gmail.com>
2025-06-08 20:02:13 +0200

    Implement JDCB(inventory & login)

--- resources/sql/setup.sql
+++ resources/sql/setup.sql
@@ -10,7 +10,29 @@ CREATE TABLE employee (
     password VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE inventory (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    wholesalerPrice DOUBLE NOT NULL,
+    available BOOLEAN NOT NULL DEFAULT 1,
+    stock INT NOT NULL
+);
+
+CREATE TABLE historical_inventory (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_product INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    wholesalerPrice DOUBLE NOT NULL,
+    available BOOLEAN NOT NULL,
+    stock INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 INSERT INTO employee (employeeId, name, password) VALUES 
 (123, 'Santi', 'test1'),
 (456, 'Jose', 'test2'),
 (789, 'Vicente', 'test3');
+
+INSERT INTO inventory (name, wholesalerPrice, available, stock) VALUES
+('Manzana', 10.00, 1, 10),
+('Fresa',    5.00,  1, 20);
--- src/dao/DaoImplJDBC.java
+++ src/dao/DaoImplJDBC.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import model.Amount;
 import model.Employee;
 import model.Product;
 
@@ -64,23 +65,103 @@ public class DaoImplJDBC implements Dao {
 	}
 
 	@Override
-	public ArrayList getInventory() {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<Product> getInventory() {
+		ArrayList<Product> products = new ArrayList<>();
+		String query = "select * from inventory";
+		if (this.connection == null) {
+            throw new IllegalStateException("Database connection is not established. Call connect() first.");
+        }
+		try (PreparedStatement ps = connection.prepareStatement(query)) { 
+			try (ResultSet rs = ps.executeQuery()) {
+				while (rs.next()) {
+					String  name      = rs.getString("name");
+					double  dblPrice  = rs.getDouble("wholesalerPrice");
+					Amount  price     = new Amount(dblPrice);
+					boolean available = rs.getBoolean("available");
+					int     stock     = rs.getInt("stock");
+
+					products.add(new Product(name, price, available, stock));
+				}
+			}
+		}  catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Error al cargar inventario", e);
+		}
+		
+		return products; 
 	}
 	@Override
-	public boolean writeInventory(ArrayList inventory) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean writeInventory(ArrayList<Product> inventory) {
+		String query = "insert into historical_inventory (id_product, name, wholesalerPrice, available, stock, created_at) values"
+				+ "(?, ?, ?, ?, ?, ?);";
+		if (this.connection == null) {
+            throw new IllegalStateException("Database connection is not established. Call connect() first.");
+        }
+		try (PreparedStatement ps = connection.prepareStatement(query)) { 	
+			for (Product product : inventory) {
+				ps.setInt(1, product.getId());
+				ps.setString(2, product.getName());
+				ps.setDouble(3, product.getWholesalerPrice().getValue());
+				ps.setBoolean(4, product.isAvailable());
+                ps.setInt(5, product.getStock());
+                ps.setTimestamp(6, new java.sql.Timestamp(System.currentTimeMillis()));
+                int results = ps.executeUpdate(); 
+			}
+
+            return true;
+		}  catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return false;
+		}
 	}
 
-	public void addProduct(Product product){
-		// TODO Auto-generated method stub
+	@Override
+	public void addProduct(Product product) {
+		String query = "insert into inventory (id, name, wholesalerPrice, available, stock) values"
+				+ "(?, ?, ?, ?, ?);";
+		if (this.connection == null) {
+            throw new IllegalStateException("Database connection is not established. Call connect() first.");
+        }
+		try (PreparedStatement ps = connection.prepareStatement(query)) { 	
+			ps.setInt(1, product.getId());
+            ps.setString(2, product.getName());
+            ps.setDouble(3, product.getWholesalerPrice().getValue());
+            ps.setBoolean(4, product.isAvailable());
+            ps.setInt(5, product.getStock());
+            int results = ps.executeUpdate(); 
+		}  catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
+	@Override
 	public void updateProduct(String name, int stock){
-		// TODO Auto-generated method stub
+		String query = "update inventory set stock = stock + ? where name = ?";
+		if (this.connection == null) {
+            throw new IllegalStateException("Database connection is not established. Call connect() first.");
+        }
+		try (PreparedStatement ps = connection.prepareStatement(query)) { 	
+			ps.setInt(1, stock);
+			ps.setString(2, name);
+			int results = ps.executeUpdate(); 
+		}  catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 	}
+	@Override
 	public void deleteProduct(String name){
-		// TODO Auto-generated method stub
+		String query = "delete from inventory where name = ?";
+		if (this.connection == null) {
+            throw new IllegalStateException("Database connection is not established. Call connect() first.");
+        }
+		try (PreparedStatement ps = connection.prepareStatement(query)) { 	
+			ps.setString(1, name);
+			int results = ps.executeUpdate(); 
+		}  catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 	}
+
 }
